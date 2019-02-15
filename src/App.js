26a28c33091fb2406768a70ef136f0339d133da1
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2019-02-15 17:16:21 +0100

    everything  enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Box, Grommet} from 'grommet'
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+  tag='header'
+  direction='row'
+  align='center'
+  background='brand'
+  pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+  elevation='medium'
+  style={{ zIndex: '1' }}
+  {...props}
+  />
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grommet theme={theme}>
+        <AppBar>
+          Hello Grommet!
+        </AppBar>
+        </Grommet>
       </div>
     );
   }
